12e9b5f4b8b9ef5c286fef685e2b0e2e6a3c4ea5
malak-abusharar <malak.g.abusharar@gmail.com>
2025-10-25 16:05:00 +0200

    Creating a Node.js API & Redux Toolkit (RTK Query)

---
+++ backend/products.js
@@ -0,0 +1,35 @@
+const products = [
+ {
+   id: 1,
+   productName: "Cup 1",
+   description:
+     "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+   price: 100,
+   imageLink: "https://res.cloudinary.com/ddt1tb3u0/image/upload/v1761391101/model4_nv2p6g.webp",
+ },
+ {
+   id: 2,
+   productName: "Cup 2",
+   description:
+     "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+   price: 200,
+   imageLink: "https://res.cloudinary.com/ddt1tb3u0/image/upload/v1761391086/model2_agkynl.jpg",
+ },
+ {
+   id: 3,
+   productName: "Cup 3",
+   description:
+     "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+   price: 300,
+   imageLink: "https://res.cloudinary.com/ddt1tb3u0/image/upload/v1761391079/model1_mfyjwg.webp",
+ },
+ {
+   id: 4,
+   productName: "Cup 4",
+   description:
+     "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+   price: 400,
+   imageLink: "https://res.cloudinary.com/ddt1tb3u0/image/upload/v1761391095/model3_m5h6uu.webp",
+ },
+];
+module.exports = products;
